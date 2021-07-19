2afaeba957bbf81acdcf621c6adfef6a3762f2e9
Joao Sousa <joao.rsousa23@gmail.com>
2021-07-19 21:53:29 +0930

    feat: add docker composer and dockerfile

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,15 @@
+import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  await mongoose.connect('mongodb://db:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }).then(() => {
+    console.log('mongodb working');
+  });
+
   app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port 3000');
   });
