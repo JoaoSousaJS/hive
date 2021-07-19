1171fd4b8b455254756ae6027a4a102df0855f2c
Joao Sousa <joao.rsousa23@gmail.com>
2021-07-19 22:28:19 +0930

    feat: add game model

---
+++ server/src/database/model/game/index.ts
@@ -0,0 +1,38 @@
+import {
+ Document, model, Model, Schema,
+} from 'mongoose';
+import { GameStatus } from '../../types/game-status';
+import { WonStatus } from '../../types/won-types';
+
+interface GameAttrs {
+  won: WonStatus
+  status: GameStatus
+}
+
+interface GameDocument extends Document {
+  won: WonStatus
+  status: GameStatus
+}
+
+interface GameModel extends Model<GameDocument> {
+  // eslint-disable-next-line no-unused-vars
+  build(attrs: GameAttrs): GameDocument
+}
+
+const gameSchema = new Schema<GameDocument, GameModel>({
+  status: {
+    type: String,
+    required: true,
+    enum: [GameStatus],
+  },
+  won: {
+    type: String,
+    enum: [WonStatus],
+  },
+});
+
+gameSchema.statics.build = () => new Game({
+  status: GameStatus.Open,
+});
+
+export const Game = model<GameDocument, GameModel>('Game', gameSchema);
---
+++ server/src/database/types/game-status.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+export enum GameStatus {
+  Open = 'Open',
+  Finished = 'Finished'
+}
